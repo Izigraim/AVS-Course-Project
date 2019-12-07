95b6d1735c7214f8b1a565ac6bf34d3a63e5c554
Izigraim <z7635332@gmail.com>
2019-12-07 23:32:45 +0300

    added receivers and updated manifest

---
+++ app/src/main/java/com/example/virus/BootReceiver.java
@@ -0,0 +1,22 @@
+package com.example.virus;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+
+/**
+ * This class is used to receive device boot event.
+ */
+public class BootReceiver extends BroadcastReceiver{
+
+	@Override
+	public void onReceive(Context context, Intent arg1) {
+		Intent msgIntent = new Intent(context, IntentServiceClass.class);
+		context.startService(msgIntent);
+		
+		AlarmManagerTXTShield alarmManager=new AlarmManagerTXTShield();
+		alarmManager.setAlarm(context);
+	}
+
+}
